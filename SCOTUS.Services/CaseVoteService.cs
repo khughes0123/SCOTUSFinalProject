19c3ff8f9e90e66c97478fe92d0a2644b7e6a75b
khughes0123 <khughes0123@gmailcom>
2021-04-11 13:04:09 -0400

    CaseVote Controller added

--- SCOTUS.Services/CaseVoteService.cs
+++ SCOTUS.Services/CaseVoteService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SCOTUS.Data;
+using SCOTUS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,66 @@ namespace SCOTUS.Services
 {
     public class CaseVoteService
     {
+        private readonly Guid _userId;
+
+        public CaseVoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateCaseVote(CaseVoteCreate model)
+        {
+            var entity = new CaseVote()
+
+            {
+                UserId = _userId,
+                CourtDecision = model.CourtDecision,
+                CourtId = model.CourtId,
+                CaseId = model.CaseId,
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.CaseVotes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CaseVoteDetail> GetCaseVotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .CaseVotes
+                        .Where(e => e.UserId == _userId)
+                        .Select(
+                            e =>
+                                new CaseVoteDetail
+                                {
+                                    CaseVoteId = e.CaseVoteId
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
+        public CaseVoteDetail GetCaseVoteById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .CaseVotes
+                        .Single(e => e.CaseVoteId == id && e.UserId == _userId);
+                return
+                    new CaseVoteDetail
+                    {
+                        CaseVoteId = entity.CaseVoteId,
+
+                    };
+            }
+        }
     }
 }
--- SCOTUS.WebMVC/Controllers/CaseVoteController.cs
+++ SCOTUS.WebMVC/Controllers/CaseVoteController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using SCOTUS.Models;
+using SCOTUS.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,57 @@ namespace SCOTUS.WebMVC.Controllers
 {
     public class CaseVoteController : Controller
     {
+
+        private CaseVoteService CreateCaseVoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new CaseVoteService(userId);
+            return service;
+        }
+
         // GET: CaseVote
         public ActionResult Index()
         {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new CaseVoteService(userId);
+            var model = service.GetCaseVotes();
+            return View(model);
+        }
+
+        //GET
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(
+            CaseVoteCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateCaseVoteService();
+
+            if (service.CreateCaseVote(model))
+
+            {
+                TempData["SaveResult"] = "Case vote has been added to our database.";
+                return RedirectToAction("Index");
+
+            }
+
+            ModelState.AddModelError("", "Case vote could not be added, please confirm all necessary information has been entered.");
+
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateCaseVoteService();
+            var model = svc.GetCaseVoteById(id);
+
+            return View(model);
         }
     }
 }
